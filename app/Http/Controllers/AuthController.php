37b96443572ea34e743d2bc70142df87884b26b5
LuisFelipePeirao <luis.peirao@unifebe.edu.br>
2025-11-03 17:30:31 -0300

    Meu primeiro projeto em Laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+
+{
+    public function login() 
+    {
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        //form validation
+        $request->validate(
+            [
+                'text_username' => ['required', 'email'],
+                'text_password' => ['required', 'min:6', 'max:16']
+            ],
+
+            //error messages
+            [
+                'text_username.required' => 'O nome de usuário é obrigatório',
+                'text_username.email' => 'O e-mail de usuário deve ser válido',
+                'text_password.required' => 'A senha de usuário é obrigatório',
+                'text_password.min' => 'A senha deve conter no pelo menos :min caracteres',
+                'text_password.max' => 'A senha deve conter no máximo :max caracteres'
+            ]
+        );
+
+        //get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        //check if user exists
+        $user = User::where('username', $username)
+                    ->where('deleted_at', NULL)
+                    ->first();
+        
+        if(!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Nome de usuário ou senha incorretos.');
+        }
+
+        //check if password is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Nome de usuário ou senha incorretos.');
+        }
+
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        //login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+            ]);
+
+        return redirect('/');
+
+
+    }
+
+    public function logout() 
+    {
+        //logout from the application
+        session()->forget('user');
+
+        return redirect()->to('/login');
+    }
+
+}
---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Services\Operations;
+
+class MainController extends Controller
+{
+    public function index() {
+        
+        //load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)
+                    ->notes()
+                    ->whereNull('deleted_at')
+                    ->get()
+                    ->toArray();
+
+        //show home view
+        return view('home', ['notes' => $notes]);
+
+    }
+
+    public function newNote() {
+        
+        return view('new_note');
+
+    }
+
+    public function newNoteSubmit(Request $request){
+
+        //validate request
+            $request->validate([
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve possuir pelo menos :min caracteres',
+                'text_title.max' => 'O título deve possuir no máximo :max caracteres',
+
+                'text_note.required' => 'A anotação é obrigatória',
+                'text_note.min' => 'A anotação deve possuir pelo menos :min caracteres',
+                'text_note.max' => 'A anotação deve possuir no máximo :max caracteres'
+
+            ]
+        );
+
+
+        //get user id
+
+        $id = session('user.id');
+
+        //create new note
+
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
+
+    }
+
+    public function editNote($id) {
+
+        $id = Operations::decryptId($id);
+
+        //load note
+
+        $note = Note::find($id);
+
+        //show edit note view
+
+        return view('edit_note', ['note' => $note]);
+
+    }
+
+    public function editNoteSubmit(Request $request) {
+        //validate request
+            $request->validate([
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve possuir pelo menos :min caracteres',
+                'text_title.max' => 'O título deve possuir no máximo :max caracteres',
+
+                'text_note.required' => 'A anotação é obrigatória',
+                'text_note.min' => 'A anotação deve possuir pelo menos :min caracteres',
+                'text_note.max' => 'A anotação deve possuir no máximo :max caracteres'
+
+            ]
+        );
+
+        //check if note id exists
+        if($request->note_id == null){
+            return redirect()->route('home');
+        }
+
+        //decrypt note id
+
+        $id = Operations::decryptId($request->note_id);
+
+        //load notes
+
+        $note = Note::find($id);
+
+        //update note
+        
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
+    }
+
+    public function deleteNote($id) {
+
+        $id = Operations::decryptId($id);
+
+        //load note
+        $note = Note::find($id);
+        
+        // show delete note confirmation
+        return view('delete_note', ['note' => $note]);
+
+    }
+
+    public function deleteConfirm($id){
+        //check if id is encrypted
+
+        $id = Operations::decryptId($id);
+
+        //load note
+
+        $note = Note::find($id);
+
+        //1. hard delete
+
+        //$note->delete();
+
+        //2. soft delete
+
+        // $note->deleted_at = date('Y-m-d H:i:s');
+        // $note->save();
+
+        //3. soft delete property SoftDeletes in model "Note"
+        $note->delete();
+
+        //4. hard delete property SoftDeletes in model "Note"
+        //$note->ForceDelete();
+
+        //redirect to home
+
+        return redirect()->route('home');
+    }
+}
